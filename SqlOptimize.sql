47848aaa05ae1073e7ab846b112deaff6b6b7594
Loy Vanich <laploy@gmail.com>
2025-06-19 20:12:04 +0700

    Add files via upload

--- SqlOptimize.sql
+++ SqlOptimize.sql
@@ -1,5 +1,4 @@
 -- Simple Query.sql: laploy, June 2025 
-
 USE LoyDB2025;
 GO
 
@@ -13,4 +12,19 @@ FROM Sales.SalesOrderDetail od
 		ON oh.CustomerID = c.CustomerID
 WHERE c.CustomerID = 11007
 GROUP BY c.CustomerID;
+GO
+
+-------------------------------------------------------
+-- Simple Query JOIN removed.sql: laploy, June 2025 
+USE LoyDB2025;
+GO
+
+SELECT 
+	oh.CustomerID,
+	SUM(LineTotal)
+FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+		ON od.SalesOrderID = oh.SalesOrderID 
+WHERE CustomerID = 11007
+GROUP BY oh.CustomerID;
 GO
