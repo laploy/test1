78266a70c4d77cccae2d78c6a082722f06dac416
Loy Vanich <laploy@gmail.com>
2025-06-19 20:07:29 +0700

    SQL Scripts
    Loy T-SQL script for article Optimization trick.

---
+++ SqlOptimize.sql
@@ -0,0 +1,16 @@
+-- Simple Query.sql: laploy, June 2025 
+
+USE LoyDB2025;
+GO
+
+SELECT 
+	c.CustomerID,
+	SUM(LineTotal)
+FROM Sales.SalesOrderDetail od
+	JOIN Sales.SalesOrderHeader oh
+		ON od.SalesOrderID = oh.SalesOrderID 
+	JOIN Sales.Customer c
+		ON oh.CustomerID = c.CustomerID
+WHERE c.CustomerID = 11007
+GROUP BY c.CustomerID;
+GO
